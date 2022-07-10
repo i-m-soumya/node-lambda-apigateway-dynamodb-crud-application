7b4221143f5d32886c2918b79131e29937fe7192
Soumya Ghosh <soumyaidmail@gmail.com>
2022-07-10 23:10:02 +0530

    SQS

---
+++ sqs_send_node.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+
+
+AWS.config.region = 'ap-south-1';
+const SQS = new AWS.SQS({})
+
+
+function sendMessage() {
+    const params = {
+        DelaySeconds: 10,
+        MessageAttributes: {
+            "Title": {
+                DataType: "String",
+                StringValue: "AWS SQS SEND MESSAGE USING NODE"
+            },
+            "Author": {
+                DataType: "String",
+                StringValue: "Soumya Ghosh"
+            },
+        },
+            MessageBody: "This is a piece of important information!",
+            QueueUrl: "https://sqs.ap-south-1.amazonaws.com/813823995644/hello-world-sqs"
+    }
+
+    SQS.sendMessage(params, (error, response) => {
+        if (error) {
+            console.log("ERROR: ", error)
+        } else {
+            console.log("SUCCESS: ", response)
+        }
+    })
+}
+sendMessage()
